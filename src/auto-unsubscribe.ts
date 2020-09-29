9f1a3fce7eff3e991e1e8b408bef5f71d7f6d01f
Alex Harasty <alex.harasty@gmail.com>
2020-09-28 20:30:07 -0400

    setup files, tsconfig

---
+++ src/auto-unsubscribe.ts
@@ -0,0 +1,40 @@
+import { Subscription } from 'rxjs';
+
+export class SubscriptionList {
+  [key: string]: Subscription;
+}
+
+export interface AutoUnsubscribeParams {
+  subscriptionListKey: string;
+}
+
+export function AutoUnsubscribe(params: AutoUnsubscribeParams) {
+  return function(constructor: any) {
+    const original = constructor.prototype.ngOnDestroy;
+
+    constructor.prototype.ngOnDestroy = function() {
+      if (params) {
+        const key = params.subscriptionListKey;
+
+        if (key) {
+          const subscriptionListKeyProp = this[key];
+
+          if (subscriptionListKeyProp instanceof SubscriptionList) {
+            for (const prop in subscriptionListKeyProp) {
+              if (prop) {
+                const property = subscriptionListKeyProp[prop];
+                if (property && typeof property.unsubscribe === 'function') {
+                  property.unsubscribe();
+                }
+              }
+            }
+          }
+        }
+      }
+
+      if (original && typeof original === 'function') {
+        original.apply(this, arguments);
+      }
+    };
+  };
+}
